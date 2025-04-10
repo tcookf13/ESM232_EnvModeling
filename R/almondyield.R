1c48e7e9150446ee003034fad0f16abdfa13a458
tcookf13 <tcook.f13@gmail.com>
2025-04-10 11:02:15 -0700

    updated almond yield function

--- R/almondyield.R
+++ R/almondyield.R
@@ -11,15 +11,13 @@
 
 #' Computes almond yield anomaly based on minimum temperature and precipitation.
 #' @param Tn2 Minimum temperature for month 2 (degrees Celsius)
-#' @param Tn1 Minimum temperature for month 1 (degrees Celsius)
 #' @param P1 Precipitation for month 1 (mm)
-#' @param P2 Precipitation for month 1 (mm)
 #' @param coeffs A vector of model coefficients (e.g., c(0.015, -0.0046, -0.07, 0.0043, 0.28)).
 #' @return Yield anomaly (ton/acre)
 #' @author Taylor Cook
 
 
-almondyield <- function(Tn2, Tn1, P1, P2) {
+almondyield <- function(Tn2,P1) {
   # Define the coefficients for the model
   coeffs <- c(0.015, -0.0046, -0.07, 0.0043, 0.28)
   
@@ -28,7 +26,7 @@ almondyield <- function(Tn2, Tn1, P1, P2) {
   }
   
   # Calculate the yield anomaly using the model
-  yield_anomaly <- coeffs[1] * Tn2 + coeffs[2] * Tn1^2 + coeffs[3] * P1 + coeffs[4] * P2^2 + coeffs[5]
+  yield_anomaly <- coeffs[1] * Tn2 + coeffs[2] * Tn2^2 + coeffs[3] * P1 + coeffs[4] * P1^2 + coeffs[5]
   
   # Return the yield anomaly
   return(yield_anomaly)
